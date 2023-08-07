7dd67b85efd537e38075b9d21e4aea35ffd21ef0
MD SOHAIL ALI <90498286+sohaill123@users.noreply.github.com>
2023-08-07 17:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// This is the boilerplate code given for you
-// You can modify this code
+// JavaScript Code (script.js)
 // Product data
 const products = [
   { id: 1, name: "Product 1", price: 10 },
@@ -11,6 +10,7 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
 
 // Render product list
 function renderProducts() {
@@ -22,16 +22,77 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+  cartList.innerHTML = ""; // Clear the previous cart items
+
+  const cartData = getCartDataFromSessionStorage();
+  cartData.forEach((cartItem) => {
+    const product = products.find((p) => p.id === cartItem.id);
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="remove-from-cart-btn" data-id="${product.id}">Remove</button>`;
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+  let cartData = getCartDataFromSessionStorage();
+  const existingItemIndex = cartData.findIndex((item) => item.id === productId);
+
+  if (existingItemIndex !== -1) {
+    cartData[existingItemIndex].quantity++;
+  } else {
+    cartData.push({ id: productId, quantity: 1 });
+  }
+
+  setCartDataToSessionStorage(cartData);
+  renderCart();
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+  let cartData = getCartDataFromSessionStorage();
+  const itemIndex = cartData.findIndex((item) => item.id === productId);
+
+  if (itemIndex !== -1) {
+    cartData.splice(itemIndex, 1);
+    setCartDataToSessionStorage(cartData);
+    renderCart();
+  }
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+  setCartDataToSessionStorage([]);
+  renderCart();
+}
+
+// Utility function to get cart data from session storage
+function getCartDataFromSessionStorage() {
+  const cartDataString = sessionStorage.getItem("cartData");
+  return cartDataString ? JSON.parse(cartDataString) : [];
+}
+
+// Utility function to set cart data to session storage
+function setCartDataToSessionStorage(cartData) {
+  sessionStorage.setItem("cartData", JSON.stringify(cartData));
+}
+
+// Event listener for adding item to cart
+productList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("add-to-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    addToCart(productId);
+  }
+});
+
+// Event listener for removing item from cart
+cartList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove-from-cart-btn")) {
+    const productId = parseInt(event.target.dataset.id);
+    removeFromCart(productId);
+  }
+});
 
 // Initial render
 renderProducts();
